ac3e1a5e936d2c9517fa349c539133316eb912d5
smichaelonline <stephaniemichael001@gmail.com>
2023-02-11 05:41:47 -0500

    Set up layout and add placeholders

--- Scrumdinger/ContentView.swift
+++ Scrumdinger/ContentView.swift
@@ -7,12 +7,9 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MeetingView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
         }
         .padding()
@@ -21,6 +18,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MeetingView()
     }
 }
